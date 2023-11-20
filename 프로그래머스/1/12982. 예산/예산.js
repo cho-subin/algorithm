8979eba9c384f35d51ffc9dbd0e7065881353bb1
cho-subin <100771469+cho-subin@users.noreply.github.com>
2023-11-21 00:42:45 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,12 +1,18 @@
 function solution(d, budget) {
-    d.sort((a, b) => a - b);
+    const dArr = d.sort((a, b) => a - b);
     let count = 0;
+    let sum = 0;
     
-    for(let i = 0; i < d.length; i++) {
-        if(d[i] > budget) break;
-
-        budget -= d[i];
-        count += 1;
+    for(let i=0; i<dArr.length; i++){
+        let test = sum+=dArr[i]
+        if(test>budget) break;
+        // if(test<=budget) {
+        count++;
+        // }
     }
+    
     return count;
 }
+
+// 1. 최대한 많은 부서의 물품을 구매해줘야 하니까 배열을 순서대로 나열하자.
+// 2. 앞에서부터 budget 수에 맞게 더한 갯수를 리턴하면 되지않을까?
