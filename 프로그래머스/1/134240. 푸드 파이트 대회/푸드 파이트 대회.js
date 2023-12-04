3e89f982807384c2abbc589516952007abd2d75e
cho-subin <100771469+cho-subin@users.noreply.github.com>
2023-12-04 23:40:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,14 +1,16 @@
-const makePersonalFood = (eachFood) => {
-    return parseInt(eachFood / 2);
-};
-
 function solution(food) {
-    return food.map(makePersonalFood).reverse().reduce((acc, cur, idx) => {
-        if (cur === 0) return acc;
-        
-        const calorie = food.length - idx - 1;
-        
-        acc = acc.padEnd(cur + acc.length, calorie);
-        return acc.padStart(cur + acc.length, calorie);
-    }, '0')
+    var answer = '';
+    
+    for(let i=1; i<food.length; i++){
+        const quotient = Math.floor(food[i]/2)
+        const repeatI = ((i)+'').repeat(quotient)
+        answer += repeatI
+    }
+    
+    const revAnswer = answer.split("").reverse().join('');
+    return answer += (0+revAnswer);
 }
+
+// 1. 홀수면 -1을 해서 2를 나누기
+// 2. index 1번째부터 나눈 값만큼 1부터 순차적으로 생성
+// 3. 그 뒤에 0과 완료된 2번을 뒤집어서 2번과 문자열 합치기
