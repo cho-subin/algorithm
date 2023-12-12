b045be193d43905a7e744566c40abb2106560e95
cho-subin <100771469+cho-subin@users.noreply.github.com>
2023-12-12 23:25:24 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -5,17 +5,13 @@ function solution(k, score) {
     for(s of score){
         if(arr.length < k){
             arr.push(s);
-            arr.sort((a,b)=>a-b);
         }
         else if(s>=Math.min(...arr)){
             arr.push(s);
             arr.sort((a,b)=>a-b);
             arr.shift();
         }
-        else if(s<Math.min(...arr)){
-            arr.sort((a,b)=>a-b);
-        }
-        answer.push(arr[0])
+        answer.push(Math.min(...arr))
     }
     return answer;
 }
