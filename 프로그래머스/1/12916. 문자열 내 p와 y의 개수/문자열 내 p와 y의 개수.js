d778f7dcd5432e2a2394f192ed26a40d8f18b20b
cho-subin <100771469+cho-subin@users.noreply.github.com>
2024-05-06 19:41:26 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,17 @@
+function solution(s){
+    let countP = 0;
+    let countY = 0;
+    
+    for(let i=0; i<s.length; i++){
+        if(s[i]==='p' || s[i]==='P'){
+            countP++;
+        }
+        if(s[i]==='y' || s[i]==='Y'){
+            countY++;
+        }
+    }
+    
+    const answer = countP === countY? true : false;
+    
+    return answer;
+}
