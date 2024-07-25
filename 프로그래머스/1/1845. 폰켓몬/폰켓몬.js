25744e3cb6d98a75b52e4744d54365680ed5a879
cho-subin <100771469+cho-subin@users.noreply.github.com>
2024-07-26 02:01:18 +0900

    [level 1] Title: 폰켓몬, Time: 1.11 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,20 +1,22 @@
 function solution(nums) {
-    let numA = new Set(nums);
-    let newNums = new Set();
+    let numsMap = new Map();
     
-    for(let elem of nums){
-        newNums.add(elem)
+    for(let i=0; i<nums.length; i++){
+        numsMap.set(nums[i],nums[i]);
     }
-    
-    let length = Array.from(newNums).length;
-    let halfOfNums = nums.length/2;
-    
-    if(length>halfOfNums){
-        return halfOfNums;
+
+    if(numsMap.size === nums.length/2 || numsMap.size > nums.length/2){
+        return nums.length/2;
     }
-    else return length;
+    else if(numsMap.size < nums.length/2){
+        return numsMap.size;
+    }  
     
-    console.log(length)
-    console.log(Array.from(length).length)
-    console.log(nums.length/2)
 }
+
+// 해시로 풀기
+// 1. 배열의 갯수의 반을 선택해야함.
+// 2. nums를 map 객체로 만듬.
+// 3. 만약에 map객체의 숫자와 배열의 반 갯수가 같으면 그 갯수 리턴
+// 4. 만약에 map객체의 길이가 배열의 반 갯수보다 크면 배열의 반 갯수 리턴
+// 5. 만약에 map객체의 길이가 배열의 반 갯수보다 작으면 map객체 길이 리턴
