c2458d20d4e4bc1bb223afbc6a71219e5ccaede5
cho-subin <100771469+cho-subin@users.noreply.github.com>
2024-08-06 18:59:03 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+    var answer = [];
+    
+    commands.forEach((e) => {
+        const sliceArray = array.slice(e[0]-1,e[1]).sort((a,b) => a-b);
+        answer.push(sliceArray[e[2]-1])
+    })
+    
+    return answer;
+}
+
+// i = 앞에서 끊을 위치 , j = 뒤에서 끊을 위치, k = 끊긴 배열에서 가져올 위치
